d285b2c1551d3a31edc429a0d919c6b352e38233
Diego Francisco Hernandez Guevara <34254918+DiegoFranciscoHG@users.noreply.github.com>
2020-02-23 22:07:05 +0100

    Connected blockchain to api. (#3)

--- BlockChainExampleCoin/CentralServer/app.py
+++ BlockChainExampleCoin/CentralServer/app.py
@@ -1,13 +1,32 @@
+import BlockChain
 from flask import Flask, jsonify, request
+from uuid import uuid4
 
 app = Flask(__name__)
 
 node_id = str(uuid4()).replace('-', '')
+blockhain = BlockChain()
 
 # Endpoint for nodes in the network to mine.
 @app.route('/mine', methods=['GET'])
 def mine():
-    return 'Mine'
+    last_block = blockchaing.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(sender = "0", recipient = node_id, quantity = 1)
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockhain.new_block(proof, previous_hash)
+
+    response = {
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+
+    return jsonify(response), 200
 
 # Endpoint for clients to make transactions.
 @app.route('/transactions', methods=['POST'])
@@ -22,7 +41,7 @@ def create_transaction():
         return 'Missing required values.', 400
 
     # Create new transaction.
-    index = "use blockchain class to create new transaction."
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['quantity'])
 
     response = {'message': f'Transaction scheduled to be in block {index}'}
 
@@ -32,8 +51,8 @@ def create_transaction():
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
-        'chain': 'add chain here.',
-        'length': 'get chaing length here.'
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
     }
 
     return jsonify(response), 200
@@ -45,14 +64,14 @@ def register_node():
     nodes_to_register = values.get('nodes')
 
     if nodes is None:
-        return "No nodes register.", 400
+        return "No nodes registered.", 400
 
     for node in nodes:
         # Register nodes.
         return 0
 
     response = {
-        'nodes': 'list nodes here.'
+        'nodes': list(blockhain.nodes),
     }
 
     return jsonify(response), 201
@@ -63,11 +82,13 @@ def consensus():
 
     if replaced:
         response = {
-            'chain': 'place new chain here.'
+            'message': 'Blockchain replaced',
+            'chain': blockchain.chain,
         }
     else:
         response = {
-            'chaing': 'place old chain here.'
+            'message': 'Blockchain conserved.',
+            'chain': blockchain.chain,
         }
 
     return jsonify(response), 200
