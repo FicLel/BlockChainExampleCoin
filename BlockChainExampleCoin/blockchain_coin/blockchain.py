8328396a1c55c81217a5d9d717952c7b2ad426ee
Diego Francisco Hernandez Guevara <34254918+DiegoFranciscoHG@users.noreply.github.com>
2020-02-24 15:07:58 +0100

    Developing (#4)
    * Created developing branch.
    
    * Added Central Server
    
    * class to make blocks done
    
    * Added server logic. (#2)
    
    * Connected blockchain to api. (#3)
    
    * Finished?
    
    * Ironed out bugs.
    
    * Fixed env.
    
    * Ironed out some bugs.
    
    * Delete lib64
    
    * Delete main.py
    
    * Improved project structure.
    
    Co-authored-by: FicLel <victorleal190@gmail.com>

---
+++ BlockChainExampleCoin/blockchain_coin/blockchain.py
@@ -0,0 +1,107 @@
+import hashlib
+import json
+import requests
+from time import time
+from urllib.parse import urlparse
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.nodes = set()
+
+        self.new_block(previous_hash = 1, proof = 100)
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+
+        while self.valid_proof(last_proof, proof) is False:
+            proof = proof + 1
+
+        return proof
+
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def new_block(self, proof, previous_hash = None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+
+        return block
+
+    def new_transaction(self, sender, recipient, quantity):
+        self.current_transactions.append({
+            'sender' : sender,
+            'recipient' : recipient,
+            'amount' : quantity,
+        })
+        
+        return self.last_block['index'] + 1
+
+    def validate_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index = current_index + 1
+
+        return True
+
+    def resolve(self):
+        peers = self.nodes
+        new_chain = None
+        max_length = len(self.chain)
+
+        for node in peers:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        primaryString = json.dumps(block, sort_keys = True).encode()
+        hashString = hashlib.sha256(primaryString).hexdigest()
+
+        return hashString
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        proof_guess = f'{last_proof}{proof}'.encode()
+        proof_guess_hash = hashlib.sha256(proof_guess).hexdigest()
+
+        return proof_guess_hash[:4] == "0000"
---
+++ BlockChainExampleCoin/blockchain_coin_server/app.py
@@ -0,0 +1,98 @@
+from ..blockchain_coin.blockchain import Blockchain
+from flask import Flask, jsonify, request
+from uuid import uuid4
+
+app = Flask(__name__)
+
+node_id = str(uuid4()).replace('-', '')
+blockchain = Blockchain()
+
+# Endpoint for nodes in the network to mine.
+@app.route('/mine', methods=['GET'])
+def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(sender = "0", recipient = node_id, quantity = 1)
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+
+    return jsonify(response), 200
+
+# Endpoint for clients to make transactions.
+@app.route('/transactions', methods=['POST'])
+def create_transaction():
+    values = request.get_json()
+
+    # Fields that are required in the requests body to create a new transaction.
+    required = ['sender', 'recipient', 'quantity']
+
+    # Check that request body has all the required fields.
+    if not all (value in values for x in required):
+        return {'error': 'Missing required values.'}, 400
+
+    # Create new transaction.
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['quantity'])
+
+    response = {'message': f'Transaction scheduled to be in block {index}'}
+
+    return jsonify(response), 201
+
+# Endpoint that returns the entire blockchain.
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+
+    return jsonify(response), 200
+
+@app.route('/register', methods=['POST'])
+def register_node():
+    values = request.get_json()
+
+    nodes_to_register = values.get('nodes')
+
+    if nodes_to_register is None:
+        return {'error': 'No nodes registered.'}, 400
+
+    for node in nodes_to_register:
+        # Register nodes.
+        return 0
+
+    response = {
+        'nodes': list(blockhain.nodes),
+    }
+
+    return jsonify(response), 201
+
+@app.route('/consensus', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve()
+
+    if replaced:
+        response = {
+            'message': 'Blockchain replaced',
+            'chain': blockchain.chain,
+        }
+    else:
+        response = {
+            'message': 'Blockchain conserved.',
+            'chain': blockchain.chain,
+        }
+
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
